58059b4472544cadad3ddfa3c59ec0679417063c
wsw <q2292901033@gmail.com>
2017-08-16 23:32:34 +0800

    update gradle

---
+++ app/src/main/java/com/example/wsw/bakingapp/data/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.example.wsw.bakingapp.data
+
+/**
+ * Created by wsw on 17-7-28.
+ */
+data class Ingredient(val quantity: Double, val measure: String, val ingredient: String) {
+}
---
+++ app/src/main/java/com/example/wsw/bakingapp/data/Step.kt
@@ -0,0 +1,8 @@
+package com.example.wsw.bakingapp.data
+
+/**
+ * Created by wsw on 17-7-28.
+ */
+data class Step(val id: Int, val shortDescription: String, val description: String,
+    val videoURL: String, val thumbnailURL: String) {
+}
